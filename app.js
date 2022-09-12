171bc1b6a3b4e40edeac87a0f18eff9d5ada1a77
Farida Fadilah <faridafadilah42807@gmail.com>
2022-09-12 13:57:51 +0700

    Membuat CRUD dengan Express

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express"); // Module Express
 const expressLayouts = require("express-ejs-layouts"); // Module Layout EJS
-const loadContact = require('./utils/contacts'); // Local Module
+const { body, validationResult, check } = require('express-validator'); // Express Validator
+const { 
+  loadContact, 
+  findContact, 
+  addContact, 
+  cekDuplikat, 
+  deleteContact, 
+  updateContact 
+}= require('./utils/contacts'); // Local Module
 
 const app = express();
 const port = 3000; //Port Server
@@ -9,10 +17,11 @@ const port = 3000; //Port Server
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use(express.static('public')); // Middleware untuk public(img,css)
+app.use(express.urlencoded({ extended: true })); // Untuk parsing body request
 
 // Url & Menampilkan FIle index.html
-app.get("/", (req, res) => {
-  res.render("index", {
+app.get('/', (req, res) => {
+  res.render('index', {
     nama: 'Farida',
     title: 'Halaman Index',
     layout: 'layouts/main-layouts'
@@ -20,8 +29,8 @@ app.get("/", (req, res) => {
 });
 
 // Url /About & Menampilkan File about.html
-app.get("/about", (req, res) => {
-  res.render("about", { 
+app.get('/about', (req, res) => {
+  res.render('about', { 
     nama: 'Farida Fadilah',
     deskripsi: 'Programmer | Doctor',
     title: 'Halaman About',
@@ -30,25 +39,106 @@ app.get("/about", (req, res) => {
 });
 
 // Url /contact & Menampilkan File Contact.html
-app.get("/contact", (req, res) => {
+app.get('/contact', (req, res) => {
   const contacts = loadContact();
-  res.render("contact", {
+  res.render('contact', {
     title: 'Halaman Contact',
     layout: 'layouts/main-layouts',
     contacts,
   });
 });
 
-// Url /contact
-app.get("/contact/:id", (req, res) => {
-  // Mengambil parameter id dan query Category
-  res.send(
-    `Product Id:  ${req.params.id} <br> Category ID: ${req.query.category}`
-  );
+// Menambah Data Contact
+app.get('/contact/add', (req, res) => {
+  res.render('create', {
+    title: 'Halaman Tambah Data',
+    layout: 'layouts/main-layouts'
+  });
+});
+
+// Proses Menyimpan Data Contact
+app.post('/contact', [
+  body('nama').custom(value => {
+      const duplikat = cekDuplikat(value);
+      if(duplikat) {
+        throw Error('Maaf, Contact yang anda masukan sudah tersedia.');
+      }
+      return true;
+    }),
+  check('email', 'Email tidak Valid').isEmail(),
+  check('nohp', 'No Hp tidak Valid').isMobilePhone('id-ID') ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('create', {
+        title: 'Halaman Tambah Data',
+        layout: 'layouts/main-layouts',
+        errors: errors.array()
+      });
+    } else {
+      addContact(req.body);
+      res.redirect('/contact');
+    }
+});
+
+// Menghapus Data Contact
+app.get('/contact/delete/:nama', (req, res) => {
+  const contact = findContact(req.params.nama);
+  if(!contact) {
+    return res.send('Maaf Nama Contact tidak Ada!');
+  }
+  deleteContact(req.params.nama);
+  res.redirect('/contact');
+});
+
+// Mengedit Data
+app.get('/contact/edit/:nama', (req, res) => {
+  const contact = findContact(req.params.nama);
+  res.render('edit', {
+    title: 'Halaman Edit Data',
+    layout: 'layouts/main-layouts',
+    contact
+  });
+});
+
+//proses Mengubah Data contact
+app.post('/contact/update', [
+  body('nama').custom((value, { req }) => {
+      const duplikat = cekDuplikat(value);
+      if (value !== req.body.oldNama && duplikat) {
+          throw new Error('Nama sudah terdaftar!');
+      }
+      return true;
+  }),
+  check('email', 'Email tidak valid!').isEmail(),
+  check('nohp', 'No HP tidak Valid!').isMobilePhone('id-ID')
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.render('edit', {
+            title: "ExpressJS",
+            layout: "layouts/main-layouts",
+            errors: errors.array(),
+            contact: req.body,
+        });
+    } else{
+        updateContact(req.body);
+        res.redirect('/contact');
+    }
+});
+
+
+// Detail Contact Berdasarkan Nama
+app.get('/contact/:nama', (req, res) => {
+  const contact = findContact(req.params.nama);
+  res.render('detail', {
+    title: 'Halaman Edit Data',
+    layout: 'layouts/main-layouts',
+    contact
+  });
 });
 
 // Url selain diatas
-app.use("/", (req, res) => {
+app.use('/', (req, res) => {
   res.status(404);
   res.send("Not Found: 404");
 });
--- utils/contacts.js
+++ utils/contacts.js
@@ -19,4 +19,48 @@ const loadContact = () => {
     return contacts;
 }
 
-module.exports = loadContact;
+// Mencari Data
+const findContact = (nama) => {
+  const contacts = loadContact();
+  const contact = contacts.find((contact) => contact.nama === nama);
+  return contact;
+}
+
+// Cek Duplikat
+const cekDuplikat = (nama) => {
+  const contacts = loadContact();
+  return contacts.find((contact) => contact.nama === nama );
+}
+
+// Simpan Data
+const saveContact = (contacts) => {
+  fs.writeFileSync(filePath, JSON.stringify(contacts, null, 2));
+}
+
+// Tambah Contact
+const addContact = (contact) => {
+  const contacts = loadContact();
+  contacts.push(contact); // Push data
+  // Save Contact
+  saveContact(contacts);
+}
+
+// Menghapus Data
+const deleteContact = (nama) => {
+  const contacts = loadContact();
+  const NewContact = contacts.filter((contact) => contact.nama !== nama);
+  // Save Contact
+  saveContact(NewContact);
+}
+
+// Mengupdate Data Contact Berdasarkan nama
+const updateContact = (contactBaru) => {
+  const contacts = loadContact();
+  //hilangkan contact lama yang namanya sama dengan oldNama
+  const filteredContacts = contacts.filter((contact) => contact.nama !== contactBaru.oldNama);
+  delete contactBaru.oldNama;
+  filteredContacts.push(contactBaru);
+  saveContact(filteredContacts);
+};
+
+module.exports = { loadContact, findContact, saveContact, addContact, cekDuplikat, deleteContact, updateContact };
